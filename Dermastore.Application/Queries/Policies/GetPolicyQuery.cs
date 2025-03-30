03177143da26550049927c6c72b8661ef95afdfb
quanggiap <nguyenquanggiap642004@gmail.com>
2025-03-31 04:55:05 +0700

    finish policy page

---
+++ Dermastore.Application/Queries/Policies/GetPolicyQuery.cs
@@ -0,0 +1,13 @@
+﻿using MediatR;
+
+namespace Dermastore.Application.Queries.Policies
+{
+    public class GetPolicyQuery : IRequest<string>
+    {
+        public string fileName { get; set; }
+        public GetPolicyQuery(string fileName)
+        {
+            this.fileName = fileName;
+        }
+    }
+}
